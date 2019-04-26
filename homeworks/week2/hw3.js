6284e61a005b9cfd0d2bf6403fe37b9808e9d823
chinyunchen <chenclaire17@gmail.com>
2019-04-26 17:02:06 +0800

    upload week2

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,10 @@
 function reverse(str) {
-  return str;
+  const arr = str.split('');
+  let answer = '';
+  for (let i = str.length; i > 0; i -= 1) {
+    answer += arr[i - 1];
+  }
+  return answer;
 }
 
 reverse('hello');
--- homeworks/week2/hw4.js
+++ homeworks/week2/hw4.js
@@ -1,5 +1,11 @@
 function printFactor(n) {
-  return n;
+  let answer = '';
+  for (let i = 0; i < n + 1; i += 1) {
+    if (n % i === 0) {
+      answer += `${i} + \n`;
+    }
+  }
+  return answer;
 }
 
 printFactor(10);
