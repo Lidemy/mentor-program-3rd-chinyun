ecb8a9a78cc01f3f984dd83a1db3f5d4fcb503a0
chinyunchen <chenclaire17@gmail.com>
2019-04-27 13:59:36 +0800

    fixed hw3,hw4

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,8 +1,7 @@
 function reverse(str) {
-  const arr = str.split('');
   let answer = '';
   for (let i = str.length; i > 0; i -= 1) {
-    answer += arr[i - 1];
+    answer += str[i - 1];
   }
   return answer;
 }
