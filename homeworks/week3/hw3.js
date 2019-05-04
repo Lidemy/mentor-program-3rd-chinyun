3f8a0c1a4bbfcb6b464775f30a033ea8040c89f2
chinyunchen <chenclaire17@gmail.com>
2019-05-04 21:17:22 +0800

    upload week3 homework

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -1,5 +1,12 @@
 function isPrime(n) {
-  console.log(n);
+  if (n >= 1 && n <= 100000) {
+    if (n === 1) return false;
+    for (let i = 2; i < n; i += 1) {
+      if (n % i === 0) return false;
+    }
+    return true;
+  }
+  return 'n should be >=1, <= 100000';
 }
 
 module.exports = isPrime;
--- homeworks/week3/hw4.js
+++ homeworks/week3/hw4.js
@@ -1,5 +1,18 @@
 function isPalindromes(str) {
-  console.log(str);
+  if (str.length < 100) {
+    const arr = [];
+    let test = '';
+    for (let i = 0; i < str.length; i += 1) {
+      arr.push(str[i]);
+    }
+    arr.reverse();
+    for (let i = 0; i < arr.length; i += 1) {
+      test += arr[i];
+    }
+    if (str === test) return true;
+    return false;
+  }
+  return str;
 }
 
 module.exports = isPalindromes;
